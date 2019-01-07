2b78be8cf7e418beb34e1da4abe8ca0280db23dd
Maniccia <mariamaniccia@gmail.com>
2019-01-06 22:57:43 -0500

    switch/case start

--- JS/liri.js
+++ JS/liri.js
@@ -3,7 +3,25 @@ require("dotenv").config();
 //var keys = require("./keys.js");
 
 //var spotify = new Spotify(keys.spotify);
+var fs = require("axios");
+var action = process.argv[2];
+var value = process.argv[3];
 
-var operand = inputString[2];
-
-console.log(operand);
+switch (action) {
+    case "concert-this":
+      concert();
+      break;
+    
+    case "spotify-this-song":
+      song();
+      break;
+    
+    case "movie-this":
+      movie();
+      break;
+    
+    case "do-what-it-says":
+      doIt();
+      break;
+    }
+    
