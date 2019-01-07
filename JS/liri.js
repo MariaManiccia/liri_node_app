675af07c62e8848afb777db1b71726c90223b824
Maniccia <mariamaniccia@gmail.com>
2019-01-06 22:39:13 -0500

    more setup

--- JS/liri.js
+++ JS/liri.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
+//var keys = require("./keys.js");
 
-var spotify = new Spotify(keys.spotify);
+//var spotify = new Spotify(keys.spotify);
 
 var operand = inputString[2];
 
