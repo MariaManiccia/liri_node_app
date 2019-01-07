6b5467d4a232f085024de6426e61458fb845ff10
Maniccia <mariamaniccia@gmail.com>
2019-01-07 18:30:35 -0500

    multiple words allowed

--- JS/liri.js
+++ JS/liri.js
@@ -2,27 +2,27 @@
 require("dotenv").config();
 
 // Main Variables
-var keys = require("./keys.js");
+//var keys = require("./keys.js");
 
-var Spotify = require('node-spotify-api');
+//var Spotify = require('node-spotify-api');
 var axios = require("axios");
 
-var spotify = new Spotify(keys.spotify);
-var axiosKey = new Axios(keys.axiosKey.key);
-var action = process.argv[2];
+//var spotify = new Spotify(keys.spotify);
+//var axiosKey = new Axios(keys.axiosKey.key);
+
 
 var nodeArgs = process.argv;
+var action = process.argv[2];
 var value = "";
 
 // Allowing multiples words 
 for (var i = 3; i < nodeArgs.length; i++) {
 
-  if (i > 2 && i < nodeArgs.length) {
+  if (i > 3 && i < nodeArgs.length) {
     value = value + "+" + nodeArgs[i];
   }
   else {
-    movieName += nodeArgs[i];
-
+    value += nodeArgs[i];
   }
 }
 
@@ -49,7 +49,7 @@ switch (action) {
 // Functions to be called
 function movie() {
 
-  axios.get("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&tomatoes=true&apikey=" + axiosKey + "").then(
+  axios.get("http://www.omdbapi.com/?t=" + value + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
     function (response) {
       console.log("The movie's title: " + response.data.Title);
       console.log("Year released: " + response.data.Year);
